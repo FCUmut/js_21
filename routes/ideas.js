3a82740e1117334e00ff452f48478787d5d06a31
Furkan Umut Ç. <umutcomalak@gmail.com>
2024-07-11 23:30:10 +0300

    commit
    commit

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const Idea = require("../models/Idea.js");
+
 const ideas = [
   {
     id: 1,
@@ -12,10 +14,17 @@ const ideas = [
 ];
 
 // Get all ideas
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // since we will connect it to server.js, we don't need to put /api etc.
   // router.get("/api/ideas", (req, res) => {
-  res.json({ success: true, data: ideas });
+  //res.json({ success: true, data: ideas });
+
+  try {
+    const ideas = await Idea.find();
+    res.json({ success: true, data: ideas });
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Something went wrong." });
+  }
 });
 
 // Get single idea
@@ -32,22 +41,37 @@ router.get("/:id", (req, res) => {
 });
 
 // Add an idea
-router.post("/", (req, res) => {
-  const idea = {
-    id: ideas.length + 1,
+// dont forget to add 'async'
+router.post("/", async (req, res) => {
+  // const idea = {
+  //   id: ideas.length + 1,
+  //   text: req.body.text,
+  //   tag: req.body.tag,
+  //   username: req.body.username,
+  //   date: new Date().toISOString().slice(0, 10),
+  // };
+
+  const idea = new Idea({
     text: req.body.text,
     tag: req.body.tag,
     username: req.body.username,
-    date: new Date().toISOString().slice(0, 10),
-  };
+  });
 
   console.log(idea);
 
-  ideas.push(idea);
+  // ideas.push(idea);
 
-  res.json({ success: true, data: idea });
+  //res.json({ success: true, data: idea });
+
   // res.send("Post success");
   // res.send(req.body.text);
+
+  try {
+    const savedIdea = await idea.save();
+    res.json({ success: true, data: savedIdea });
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Something went wrong." });
+  }
 });
 
 // Update idea
