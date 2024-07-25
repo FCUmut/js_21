28b3e5db4b4100df5a50c18dbd50b37594d1a339
Furkan Umut Ç. <umutcomalak@gmail.com>
2024-07-25 22:28:32 +0300

    commit
    commit

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -16,8 +16,12 @@ class IdeaForm {
     this._formModal.innerHTML = ` <form id="idea-form">
           <div class="form-control">
             <label for="idea-text">Enter a Username</label>
-            <input type="text" name="username" id="username" />
-          </div>
+            <input type="text" name="username" id="username" value="${
+              localStorage.getItem("username")
+                ? localStorage.getItem("username")
+                : ""
+            }" />
+          </div> 
           <div class="form-control">
             <label for="idea-text">What's Your Idea?</label>
             <textarea name="text" id="idea-text"></textarea>
@@ -36,6 +40,18 @@ class IdeaForm {
   async handleSubmit(e) {
     e.preventDefault();
 
+    if (
+      !this._form.elements.text.value ||
+      !this._form.elements.tag.value ||
+      !this._form.elements.username.value
+    ) {
+      alert("Please fill all blank fields.");
+      return;
+    }
+
+    // Save user to local storage
+    localStorage.setItem("username", this._form.elements.username.value);
+
     const idea = {
       text: this._form.elements.text.value, // textarea named text
       tag: this._form.elements.tag.value,
@@ -53,6 +69,8 @@ class IdeaForm {
     this._form.elements.text.value = "";
     this._form.elements.tag.value = "";
     this._form.elements.username.value = "";
+
+    this.render();
 
     document.dispatchEvent(new Event("closemodal"));
   }
