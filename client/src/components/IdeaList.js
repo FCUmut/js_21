a0ae835d5d064f7cbeb3cd5ee4c7476b73894464
Furkan Umut Ç. <umutcomalak@gmail.com>
2024-07-30 22:49:35 +0300

    commit
    commit

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -31,6 +31,16 @@ class IdeaList {
     this._validTags.add("inventions");
   }
 
+  addEventListeners() {
+    this._ideaListEl.addEventListener("click", (e) => {
+      if (e.target.classList.contains("fa-times")) {
+        e.stopImmediatePropagation();
+        const ideaId = e.target.parentElement.parentElement.dataset.id;
+        this.deleteIdea(ideaId);
+      }
+    });
+  }
+
   async getIdeas() {
     try {
       const res = await IdeasApi.getIdeas();
@@ -39,6 +49,17 @@ class IdeaList {
       console.log(this._ideas);
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async deleteIdea(ideaId) {
+    try {
+      // Delete from server
+      const res = await IdeasApi.deleteIdea(ideaId);
+      this._ideas.filter((idea) => idea._id !== ideaId);
+      this.getIdeas();
+    } catch (error) {
+      alert("You can not delete this resource.");
     }
   }
 
@@ -62,9 +83,14 @@ class IdeaList {
     this._ideaListEl.innerHTML = this._ideas
       .map((idea) => {
         const tagClass = this.getTagClass(idea.tag);
+        // if you are different user, you will not even see delete button
+        const deleteBtn =
+          idea.username === localStorage.getItem("username")
+            ? `<button class="delete"><i class="fas fa-times"></i></button>`
+            : "";
         return `
-         <div class="card">
-          <button class="delete"><i class="fas fa-times"></i></button>
+         <div class="card" data-id="${idea._id}">
+          ${deleteBtn}
           <h3>
            ${idea.text}
           </h3>
@@ -77,6 +103,7 @@ class IdeaList {
         `;
       })
       .join("");
+    this.addEventListeners();
   }
 }
 
